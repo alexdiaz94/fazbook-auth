340bd5d7ee6fba2299d60c85f1187541bfb16855
alex diaz <alexdiaz094@gmail.com>
2017-01-28 22:59:05 -0500

    add auth routes

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -5,3 +5,30 @@ function comparePass(userPassword, databasePassword) {
   return bcrypt.compareSync(userPassword, databasePassword);
 }
 //function that will use bcrypt to compare passwords
+function loginRedirect(req, res, next) {
+  if (req.user)
+    return res.status(401).json(
+      { status: 'You are already logged in' }
+      );
+    return next();
+}
+//function to show redirect a logged in user to their user profile
+// page if they're already logged in. We don't want logged
+// in users accessing the register page
+function createUser(req, res) {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+
+  return models.User.create({
+    username: req.body.username,
+    password: hash,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    dob: req.body.dob
+  }).then(() => {
+    res.redirect('/');
+  });
+}
+//this function will create the user based on criteria
+// and encrypt password
